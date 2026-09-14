87d5fe4e40f46f9969b88c3f5fa36a9a5083b798
Chandan <ckmallick2005@gmail.com>
2026-09-14 06:28:18 +0000

    part finc

---
+++ c-shell/include/snoop.h
@@ -0,0 +1,5 @@
+#ifndef SNOOP_H
+#define SNOOP_H
+#include "lexer.h"
+void run_snoop(Token *tokens);
+#endif
---
+++ c-shell/include/spy.h
@@ -0,0 +1,5 @@
+#ifndef SPY_H
+#define SPY_H
+#include "lexer.h"
+void run_spy(Token *tokens);
+#endif
